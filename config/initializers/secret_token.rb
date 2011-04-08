31d551600a5eac2f4f2b10995249fc6c89a66798
Freddie <freddieboi@gmail.com>
2011-04-08 15:59:20 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenIDTest::Application.config.secret_token = '3d41ce931350d6c40a34576949eae5d368e297e764a92e0ab3192fcbb245c8c2c7b7c830581cdc3b64e220dd40e27b0d6616eaa4b0971417c7b99743577e3c8f'
